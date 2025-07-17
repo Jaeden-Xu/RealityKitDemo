4cb74afa8996b5a1a28a3468e66cdcc6262af220
jaeden-xu <jzxu98@outlook.com>
2025-07-17 10:47:54 +0800

    Initial Commit

---
+++ RealityKitDemoTests/RealityKitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKitDemoTests.swift
+//  RealityKitDemoTests
+//
+//  Created by JaedenXu on 2025/7/17.
+//
+
+import Testing
+@testable import RealityKitDemo
+
+struct RealityKitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
